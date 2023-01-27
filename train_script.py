2ce5dabfbe28a88fba3b220a7cec98fb8920670e
Matt Sampson <matt.sampson@princeton.edu>
2023-01-26 22:01:35 -0500

    altered save path

--- train_script.py
+++ train_script.py
@@ -42,8 +42,19 @@ from tqdm import tqdm
 from model import ScoreNet
 # test we can find correct device
 from jax.lib import xla_bridge
-print(f'Device used is: {xla_bridge.get_backend().platform}')
 import argparse
+
+# Scorenet asci art
+print(' _____                         _   _        _   ')
+print('/  ___|                       | \ | |      | |  ')
+print('\ `--.   ___  ___   _ __  ___ |  \| |  ___ | |_ ')
+print(' `--. \ / __|/ _ \ |  __|/ _ \| . ` | / _ \| __|')
+print('/\__/ /| (__| (_) || |  |  __/| |\  ||  __/| |_ ')
+print('\____/  \___|\___/ |_|   \___|\_| \_/ \___| \__|')
+print('Generating galaxies from noise with deep learning')     
+print('             <>  Matt Sampson  <>')                                       
+
+#print(f'Device used: {xla_bridge.get_backend().platform}')
 
 # parse in the image size to train on from the command line
 # Parse arguements
@@ -248,7 +259,6 @@ best_loss   = 1e15
 epoch_loss  = 0
 
 # print message with training details
-print()
 print('----------------------------------------')
 print(f'   training ScoreNet on {len(training_data)} images')
 print('----------------------------------------')

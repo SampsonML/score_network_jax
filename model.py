f51269970236b8e240ee66ddc5e39da00619c569
Matt Sampson <matt.sampson@princeton.edu>
2023-01-28 19:52:41 -0500

    removed conv3by3 bias

--- model.py
+++ model.py
@@ -82,7 +82,8 @@ def ncsn_conv1x1(x, out_planes, stride=1, bias=True, dilation=1, init_scale=1.):
                    bias_init=bias_init)(x)
   return output
 
-def ncsn_conv3x3(x, out_planes, stride=1, bias=True, dilation=1, init_scale=1.):
+# Changed bias to false
+def ncsn_conv3x3(x, out_planes, stride=1, bias=False, dilation=1, init_scale=1.):
   """3x3 convolution with PyTorch initialization. Same as NCSNv1/NCSNv2."""
   kernel_init = jnn.initializers.variance_scaling(1 / 3 * init_scale, 'fan_in',
                                                   'uniform')
@@ -95,8 +96,8 @@ def ncsn_conv3x3(x, out_planes, stride=1, bias=True, dilation=1, init_scale=1.):
                    padding='SAME',
                    use_bias= bias,
                    kernel_dilation=(dilation, dilation),
-                   kernel_init=kernel_init,
-                   bias_init=bias_init)(x)
+                   kernel_init=kernel_init)(x)
+                   #bias_init=bias_init)(x)
   return output
 
 # --------------------------------------------------------- #
